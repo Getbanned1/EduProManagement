da6fcc17b7936855b72c295699e2c48ac55effdc
Getbanned <epihinmaxim876@gmail.com>
2026-02-07 14:30:13 +0300

    added tests

---
+++ EduProTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using EduProManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EduProTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+    public class CourseServiceTests
+    {
+        //private AppDbContext GetDbContext()
+        //{
+        //    var options = new DbContextOptionsBuilder<EduP>()
+        //        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+        //    .Options;
+
+        //    var context = new AppDbContext(options);
+
+        //    context.Courses.AddRange(
+        //        new Course { Name = "C# Basics", StartDate = new DateTime(2024, 1, 10) },
+        //        new Course { Name = "ASP.NET", StartDate = new DateTime(2024, 3, 1) },
+        //        new Course { Name = "Java", StartDate = new DateTime(2023, 12, 5) }
+        //    );
+
+        //    context.SaveChanges();
+        //    return context;
+        //}
+    }
+
+}
